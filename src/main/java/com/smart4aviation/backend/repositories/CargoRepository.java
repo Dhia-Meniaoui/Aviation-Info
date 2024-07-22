c2257c8d335d15d821a67f0946d40cfc4f37b440
Dhia-Meniaoui <diaminiaoui@gmail.com>
2024-07-22 13:30:24 +0100

    second commit

---
+++ src/main/java/com/smart4aviation/backend/repositories/CargoRepository.java
@@ -0,0 +1,24 @@
+package com.smart4aviation.backend.repositories;
+
+import com.smart4aviation.backend.entities.CargoEntity;
+import com.smart4aviation.backend.exceptions.CargoEntityNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CargoRepository {
+
+    public final List<CargoEntity> cargoEntities = new ArrayList<>();
+    public void save(List<CargoEntity> cargotList) {
+        cargoEntities.addAll(cargotList);
+    }
+
+    public CargoEntity findCargoEntityByFlightId(Long flightId) {
+        return cargoEntities.stream()
+                .filter(x -> x.getFlightId().equals(flightId))
+                .findFirst()
+                .orElseThrow(() -> new CargoEntityNotFoundException("this flight has No Consignments"));
+    }
+}
